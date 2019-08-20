442fae0a02291b07d26bc6b0e0af11d2cf44ae6a
mohdafzalwl <mohammed@wavelabs.in>
2019-08-20 15:14:59 +0530

    adeed put

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/ths19d', {useNewUrlParser: true});
 
+//import the model here 
 module.exports.PRODUCTS = require('./products')
--- routes/products.js
+++ routes/products.js
@@ -19,5 +19,28 @@ router.post('/', (req,res)=>{
     .catch((err)=> res.send(err))
 })
 
+//Get - /products/:id
+//Get a single a products
+router.get('/:id', (req,res)=>{
+    const productId = req.params.id
+    db.PRODUCTS.findOne({_id:productId})
+    .then((data)=> res.json(data))
+    .catch((err)=> res.send(err))
+})
+//PUT - /products/:id
+//Update a product - findOneAndUpdate
+router.put('/:id',(req,res)=>{
+    db.PRODUCTS.findOneAndUpdate({_id:req.params.id}, req.body)
+    .then((data)=> res.json(data))
+    .catch((err)=> res.send(err))
+})
+
+//DELETE - /products/:id
+//Delete a product - findOneAndRemove
+router.delete('/:id', (req,res)=>{
+   db.PRODUCTS.findOneAndRemove({_id:req.params.id})
+   .then(()=> res.send("product deleted"))
+   .catch((err)=> res.send(err))
+})
 
 module.exports = router
